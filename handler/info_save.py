ad3bd4f8c07d583c609e6978b3a72919d697f30b
Ogurche <337398@edu.itmo.ru>
2024-05-12 15:38:18 +0500

    Sheduler test version add

--- handler/info_save.py
+++ handler/info_save.py
@@ -7,6 +7,13 @@ from sys_files.bot_creation import dp
 async def start_cmd (message: types.Message):
     await message.answer('Бот для поиска авиабилетов')
 
+async def simple_answer(message:types.Message):
+    await message.answer('Dayte denyg')
 
 async def register_info (dp:Dispatcher):
     dp.message.register(start_cmd,Command('start') )
+    #
+    # Сюда надо еще накидать хендлеров 
+    #
+    #
+    dp.message.register(simple_answer)
--- startup_bot.py
+++ startup_bot.py
@@ -20,10 +20,21 @@ def logging_bot():
 # async def cmd_start(message: types.Message):
 #     await message.answer("Hello!")
 
+async def schedule_daily_task():
+    while True:
+        print ('date')
+        await asyncio.sleep(5)
+
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await register_info(dp)
-    await dp.start_polling(bot,skip_update=True)
+
+    task2 = asyncio.create_task(dp.start_polling(bot, skip_update=True))
+    task3 = asyncio.create_task(schedule_daily_task())
+    print ('Бот запущен')
+    # Wait for all tasks to complete
+
+    await asyncio.gather(task2, task3)
 
 if __name__ == '__main__':
     logging_bot()
